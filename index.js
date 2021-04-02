668f9ae49bd776d55c6331673962ba9284000e04
Ryan Helm <rjhelm@github.io>
2021-04-01 21:50:22 -0600

    added the question skeleton to the index before adding functions after tests

---
+++ index.js
@@ -0,0 +1,47 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateTeam = require('./src/template');
+const yourTeam = [];
+
+const questions = [
+    {
+        type: 'list',
+        name: 'role',
+        message: "What is the employee's role?",
+        choices: [] 
+    },
+    {
+        type: 'input',
+        name: 'firstName',
+        message: ''
+        
+    },
+    {
+        type: 'input',
+        name: 'lastName',
+        message: ''
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: ''
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: ''
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: ''
+    },
+    {
+        type: 'confirm',
+        name: 'addEmployee',
+        message: 'Would you like to add another employee to your team?',
+        default: true
+    }
+
+
+]
