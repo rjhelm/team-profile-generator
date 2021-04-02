7fbdf5faf0cfef5b0f85790f4eee3d5c8dacff8a
Ryan Helm <rjhelm@github.io>
2021-04-02 01:26:10 -0600

    created remaining tests for the manager class

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,4 +1,4 @@
-const { expect } = require('@jest/globals');
+const { expect, test } = require('@jest/globals');
 const Manager = require('../lib/Manager');
 const { expectToBe, expectNumber, expectString } = require('../utils/expect');
 
@@ -12,4 +12,28 @@ test('Creates manager as an object', () => {
     expectToBe(manager.role, 'Manager');
     expectNumber(manager.officeNumber);
     expectToBe(manager.icon, 'fas fa-mug');
+});
+
+test("Get manager's name", () => {
+    expectString(manager.getName(), `${manager.firstName} ${manager.lastName}`);
+});
+
+test("Get manager's id", () => {
+    expectString(manager.getId(), `${manager.id}`);
+});
+
+test("Get manager's email", () => {
+    expectString(manager.getEmail(), manager.email);
+});
+
+test("Get manager's role", () => {
+    expectString(manager.getRole(), manager.role);
+});
+
+test("Get manager's office number", () => {
+    expectString(manager.getOfficeNumber(), `${manager.officeNumber}`);
+});
+
+test("Get manager's icon", () => {
+    expectString(manager.getIcon(), manager.icon);
 });
