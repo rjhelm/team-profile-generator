fd13c91476a4c8754e295a1a6e6054cc386b5d5c
Ryan Helm <rjhelm@github.io>
2021-04-02 20:30:10 -0600

    created function for the employee role choices so that specific role can be picked by user

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateTeam = require('./src/template');
-const yourTeam = [];
+const generate = require('./src/template');
+const nameFormat = require('./utils/name-format');
+const allEmployees = [];
 
 const questions = [
     {
         type: 'list',
         name: 'role',
         message: "What is the employee's role?",
-        choices: [] 
+        choices: () => {
+            if (allEmployees.some(employee => employee.role === 'Manager')) {
+                return ['Engineer', 'Intern']
+            } else {
+                return ['Manager', 'Engineer', 'Intern']
+            }
+        }
     },
     {
         type: 'input',
