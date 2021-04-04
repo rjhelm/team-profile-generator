b828c7b191ee53342d4da98787a0688e63724e0d
Ryan Helm <rjhelm@github.io>
2021-04-03 23:06:25 -0600

    bootstap errors caused switch to bulma for testing

--- src/template.js
+++ src/template.js
@@ -5,7 +5,7 @@ const Manager = require('../lib/Manager');
 const nameFormat = require('../utils/name-format'); /* Helps ensure that the name user inputs is inserted in proper format */
 
 const addEmployee = employeeInfo => {
-    let cards = '';
+    let teamCards = '';
 
     employeeInfo.forEach(employee => {
         const { firstName, lastName, id, role } = employee;
@@ -29,23 +29,29 @@ const addEmployee = employeeInfo => {
                 addTeamate = new Employee(nameFormat(firstName), nameFormat(lastName), id);
         };
 
-        cards += `
-            <div class="card team-card mr-4 ml-4 mb-3">
-                <div class="card-body">
-                    <div class="card-header text-center">
-                        <h2 class="card-title">${addTeamate.getName()}</h2>
-                        <h3 class="-title">${addTeamate.getRole()}</h3>
-                    </div>
+        teamCards += `
+            <div class="column is-one-quarter">
+                <div class="card">
+                    <div class="card-content">
+                        <div class="media">
+                            <div class="media-left">
+                                ${addTeamate.getIcon()}
+                            </div>
+                            <div class="media-content">
+                                <p class="title is-5">${addTeamate.getName()}</p>
+                                <p class="subtitle is-5">${addTeamate.getRole()}</p>
+                            </div>
+                        </div>
                 
-                    <div class="list-group">
-                        <li class="list-group-item">${addTeamate.getId()}<br />
+                        <div class="content">
+                        <p>${addTeamate.getId()}<br />
                             ${addTeamate.getEmail()}<br />
-                         ${userInfo}</li>
+                         ${userInfo}</p>
                     </div>
                 </div>
             </div>`
     });
-    return cards;
+    return teamCards;
 };
 
 const generate = userTemplate => {
@@ -58,28 +64,23 @@ const generate = userTemplate => {
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <meta http-equiv="X-UA-Compatible" content="ie=edge">
             <title>My Team Profile</title>
-                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-                    integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-                <link href="https://fonts.googleapis.com/css?family=Merriweather|Muli:300" rel="stylesheet">
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
-                <link rel="stylesheet" href="./utils/style.css">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.1/css/bulma.min.css" />
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
         </head>
 
         <body>
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12 jumbotron mb-3 team-heading">
-                    <h1 class="text-center">My Team</h1>
+            <section class="team is-primary is-bold">
+                <div class="team-body">
+                    <div class="container>
+                        <h1 class="title">Dream Team</h1>
+                    </div>
                 </div>
-            </div>
-        </div>
-        <div class="container"
-            <div class="row">
-                <div class="col-12 d-flex justify-content-center mt-5">
+            </section>
+            <main class="m-6">
+                <div class="columns is-flex is-flex-wrap-wrap is-flex-direction-row is-justify-content-center">
                     ${addEmployee(userTemplate)}
                 </div>
-            </div>
-        </div>    
+            </main>
         </body>
         </html>
     `;
