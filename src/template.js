f9f8be61ad07a86f26bd89d121f8dfd10eec449e
Ryan Helm <rjhelm@github.io>
2021-04-04 20:58:31 -0600

    tweaked layout for cleaner html generated

--- src/template.js
+++ src/template.js
@@ -30,26 +30,26 @@ const addEmployee = employeeInfo => {
         };
 
         teamCards += `
-            <div class="column is-one-quarter">
-                <div class="card">
-                    <div class="card-content">
-                        <div class="media">
-                            <div class="media-left">
-                                ${addTeamate.getIcon()}
-                            </div>
-                            <div class="media-content">
-                                <p class="title is-5">${addTeamate.getName()}</p>
-                                <p class="subtitle is-5">${addTeamate.getRole()}</p>
-                            </div>
-                        </div>
-                
-                        <div class="content">
-                        <p>${addTeamate.getId()}<br />
-                            ${addTeamate.getEmail()}<br />
-                         ${userInfo}</p>
+        <div class="column is-one-quarter-desktop">
+        <div class="card">
+            <div class="card-content">
+                <div class="media">
+                    <div class="media-left has-text-link-dark">
+                        ${addTeamate.getIcon()}
+                    </div>
+                    <div class="media-content">
+                        <p class="title is-4">${addTeamate.getName()}</p>
+                        <p class="subtitle is-6">${addTeamate.getRole()}</p>
                     </div>
                 </div>
-            </div>`
+                <div class="content">
+                    <div class="box">${addTeamate.getId()}</div>
+                    <div class="box">${addTeamate.getEmail()}</div>
+                    <div class="box">${userInfo}</div>
+                </div>
+            </div>
+        </div>
+    </div>`  
     });
     return teamCards;
 };
@@ -64,24 +64,28 @@ const generate = userTemplate => {
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <meta http-equiv="X-UA-Compatible" content="ie=edge">
             <title>My Team Profile</title>
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.1/css/bulma.min.css" />
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css">
         </head>
 
         <body>
-            <section class="team is-primary is-bold">
-                <div class="team-body">
-                    <div class="container>
-                        <h1 class="title">Dream Team</h1>
+
+            <section class="hero is-primary is-bold">
+                <div class="hero-body has-background-link-dark">
+                    <div class="container">
+                        <h1 class="title has-text-centered">Dream Team</h1>
                     </div>
                 </div>
             </section>
+
             <main class="m-6">
-                <div class="columns is-flex is-flex-wrap-wrap is-flex-direction-row is-justify-content-center">
+            <div class="columns is-flex is-flex-wrap-wrap is-flex-direction-row is-justify-content-center">
                     ${addEmployee(userTemplate)}
                 </div>
             </main>
+
         </body>
+
         </html>
     `;
 }
